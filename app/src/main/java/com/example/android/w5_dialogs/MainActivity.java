f66d68d74bab7b816c70edad484254693e5df86e
priyankabp <pbphapale@gmail.com>
2017-02-20 18:15:48 -0500

    Add Date picker and alert dialog.

--- app/src/main/java/com/example/android/w5_dialogs/MainActivity.java
+++ app/src/main/java/com/example/android/w5_dialogs/MainActivity.java
@@ -1,17 +1,48 @@
 package com.example.android.w5_dialogs;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_date_ma)
+    Button dateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // copied from https://github.com/JakeWharton/butterknife
+        ButterKnife.bind(this);
+        dateBtn.setOnClickListener(new MyLsntr());
+    }
+
+    class MyLsntr implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+                        }
+                    }, 0, 0, 0);
+            datePickerDialog.show();
+
+        }
+    }
+
+    public void showAlert(View view) {
+
     }
 
     public void showDate(View view) {
@@ -19,11 +50,11 @@ public class MainActivity extends AppCompatActivity {
         alertbuilder.setTitle("The title of the alert")
                 .setMessage("Message body to go int this area...")
                 .setPositiveButton("Agreee", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-            }
-        });
+                    }
+                });
         alertbuilder.show();
     }
 }
